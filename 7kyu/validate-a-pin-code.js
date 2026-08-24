179b6d4c7fd5e77682e9eea736723144444ca3b1
sladebdev <sladejbrasuell@gmail.com>
2026-08-24 17:30:40 -0500

    added another users solution that solves the problem with one single regex.test() line

--- 7kyu/validate-a-pin-code.js
+++ 7kyu/validate-a-pin-code.js
@@ -13,3 +13,21 @@ function validatePIN (pin) {
     return false
   }
 }
+
+
+//daniel-mar's solution that includes a regex reference on how to solve the problem using one regext.test() line
+
+/* for my future referencing,
+
+  ^ = start of a line, 
+  \d = [0-9], {4} specifies 4 digits, 
+  {6} specifies 6 digits, $ - end of a line
+  
+  first alternative ^\d{4}$ - equivalent to [0-9], matches exactly 4 digits.
+  second alternative ^\d{6}$ - "", matches previous token exactly 6 digits.
+  
+   use RegEx .test to search for these characters stored.
+   
+*/ 
+
+const validatePIN = pin => /^\d{4}$|^\d{6}$/.test(pin);
